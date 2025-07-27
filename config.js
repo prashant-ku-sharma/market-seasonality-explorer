2bea23a58526d9ef7465a60ec2be9676879edc40
Prashant Sharma <prashant.sharma@agrostar.in>
2025-07-27 15:18:13 +0530

    project setup complete

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+  FMP_API_KEY: process.env.FMP_API_KEY || "",
+};
+
+export default config;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    FMP_API_KEY: process.env.FMP_API_KEY,
+  },
+};
 
 export default nextConfig;
